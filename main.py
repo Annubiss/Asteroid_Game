28fcabd62d04571cb820e7f558a3ab402688917d
Annubiss <adilmadra9@gmail.com>
2025-11-06 18:29:51 +0200

    fsdsd

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame
 from constants import *
 from logger import log_state 
-pygame.init()  # Initialize pygame to avoid errors when accessing pygame types
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  
+
 
 def main():
+    pygame.init() # Initialize pygame to avoid errors when accessing pygame types
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         log_state()
-        screen.fill("black")
+
+        screen.fill((0, 0, 0))
+
         pygame.display.flip()
+
 if __name__ == "__main__":
     main()
