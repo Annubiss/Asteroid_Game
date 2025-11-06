4c7365635ed11b9fc66a26c16f79e8eed0a56df1
Annubiss <adilmadra9@gmail.com>
2025-11-06 17:48:09 +0200

    "Made GUI window appear"

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+from logger import log_state 
+pygame.get_init()  # Initialize pygame to avoid errors when accessing pygame types
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+if __name__ == "__main__":
+    main()
